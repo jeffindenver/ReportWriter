1d0037a591d31c401a4b65667bf3a4d7e86b5ae2
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-02-07 15:01:54 -0600

    Extracted DurationUtility class from QueueByDateReport class.

---
+++ src/com/emscrm/DurationUtility.java
@@ -0,0 +1,24 @@
+package com.emscrm;
+
+import java.time.Duration;
+
+class DurationUtility {
+
+    private DurationUtility() {
+    }
+
+    static double toFractionOfDay(String duration) {
+        System.out.println("In method toFractionOfDay. Duration equals " + duration);
+        String[] v = duration.split(":");
+        Duration someTime = Duration.ZERO;
+        someTime = someTime.plusHours(Long.parseLong(v[0]));
+        someTime = someTime.plusMinutes(Long.parseLong(v[1]));
+        someTime = someTime.plusSeconds(Long.parseLong(v[2]));
+
+        double totalTime = someTime.toMillis() / 1000.0;
+
+        //Excel represents time as a fraction. 0.25 is six hours or 6:00 AM
+        int secondsInADay = 86400;
+        return totalTime / secondsInADay;
+    }
+}
