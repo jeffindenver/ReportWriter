a60321ba1558304742c7097da13e3783c14a072e
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-02-21 14:07:22 -0600

    Added ReportTest class

---
+++ test/com/emscrm/ReportTest.java
@@ -0,0 +1,61 @@
+package com.emscrm;
+
+import com.emscrm.reportTypes.DefaultQBD;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReportTest {
+    private List<String> testList;
+    private Report report = new DefaultQBD();
+
+    @SuppressWarnings("unchecked")
+    @BeforeEach
+    void setUp() {
+        Model model = new Model(report);
+        testList = Collections.EMPTY_LIST;
+        try {
+            String inputFilename = "testInputFile.xls";
+            testList = model.readXlsFileToList(inputFilename);
+        } catch(InvalidFormatException | IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    @Test
+    void getMatchingLine() {
+        String matcher = "CPaT Chat";
+        ListFilter listFilter = new ListFilter();
+        List<String> filteredList = listFilter.filterByLength(2, testList, "\t");
+        String result = report.getMatchingLine(filteredList, matcher);
+
+        filteredList.forEach(System.out::println);
+
+        System.out.println(result);
+        assertTrue(result.contains(matcher));
+        String[] results = result.split("\t");
+        assertTrue(results.length > 2);
+    }
+
+    @Test
+    void getSummary() {
+        String summary = report.getSummary(testList);
+        System.out.println(summary);
+        assertTrue(summary.contains("Grand Total:"));
+    }
+
+    @Test
+    void openWorkbook() {
+        try{
+            report.openWorkbook();
+        } catch (InvalidFormatException | IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
