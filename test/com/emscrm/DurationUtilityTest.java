05943c17541d8fd279b5fc8ef88d5070a4522b33
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-02-21 10:59:05 -0600

    Added DurationUtility test class

---
+++ test/com/emscrm/DurationUtilityTest.java
@@ -0,0 +1,25 @@
+package com.emscrm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DurationUtilityTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void toFractionOfDay() {
+
+        String duration = "01:01:01";
+        double expectedDuration = (3600.0 + 60.0 + 1.0) / 86400.0;
+        double calculatedDuration = DurationUtility.toFractionOfDay(duration);
+        double delta = .0001;
+
+        assertEquals(expectedDuration, calculatedDuration, delta);
+
+    }
+}
