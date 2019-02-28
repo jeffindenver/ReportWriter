79a8b12f64a78817eddfbbf6ad8e191d7fc45d2e
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-02-28 09:59:49 -0600

    CellOne report got two new tables. Added two new classes to handle them.

---
+++ src/com/emscrm/reportTypes/CellOne_Subtotal_WTD_QBD.java
@@ -0,0 +1,38 @@
+package com.emscrm.reportTypes;
+
+import com.emscrm.QueueByDateReport;
+
+import java.util.Map;
+
+/**
+ * @author JShepherd
+ */
+public final class CellOne_Subtotal_WTD_QBD extends QueueByDateReport {
+
+    private static final String reportName = "Cellular One Subtotal WTD";
+    private final Map<String, String> weekToDateTableNames = Map.of("Grand Total:", "SubtotalWTD");
+
+    public CellOne_Subtotal_WTD_QBD() {
+        this.weeklyReportFilename = "s:\\reports\\call centers\\Cellular One of NE Arizona\\CellOne Daily.xlsx";
+    }
+
+    @Override
+    public boolean isSingleLineTable() {
+        return true;
+    }
+
+    @Override
+    public Map<String, String> getTableNames() {
+        return weekToDateTableNames;
+    }
+
+    @Override
+    public String getWeeklyReportFilename() {
+        return weeklyReportFilename;
+    }
+
+    @Override
+    public String toString() {
+        return reportName;
+    }
+}
