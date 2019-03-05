dffe88a838d28cf170aff30ee283c2d74a567ead
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-03-05 10:05:06 -0600

    Changed output file path from the test file to actual file.

--- src/com/emscrm/reportTypes/Three_DX_Calls_QS.java
+++ src/com/emscrm/reportTypes/Three_DX_Calls_QS.java
@@ -10,7 +10,7 @@ public class Three_DX_Calls_QS extends QueueSummaryReport {
     private static final Map<String, String> tableNames = Map.of("Summary", "CallsTable");
 
     public Three_DX_Calls_QS() {
-        weeklyReportFilename = "s:\\reports\\call centers\\3D Connexion\\3Dconnexion Weekly Stats 2019 back.xlsx";
+        weeklyReportFilename = "s:\\reports\\call centers\\3D Connexion\\3Dconnexion Weekly Stats 2019.xlsx";
     }
 
     @Override
--- src/com/emscrm/reportTypes/Three_DX_Email_QS.java
+++ src/com/emscrm/reportTypes/Three_DX_Email_QS.java
@@ -10,7 +10,7 @@ public class Three_DX_Email_QS extends QueueSummaryReport {
     private static final Map<String, String> tableNames = Map.of("Summary", "EmailTable");
 
     public Three_DX_Email_QS() {
-        weeklyReportFilename = "s:\\reports\\call centers\\3D Connexion\\3Dconnexion Weekly Stats 2019 back.xlsx";
+        weeklyReportFilename = "s:\\reports\\call centers\\3D Connexion\\3Dconnexion Weekly Stats 2019.xlsx";
     }
 
     @Override
