a389aff388989508a9fcfece28f60aaef718f032
Jeff Shepherd <jeffindenver2002@yahoo.com>
2019-03-20 14:30:19 -0500

    Added class for the IVR report

---
+++ src/com/emscrm/reportTypes/ContractorSupportQS.java
@@ -0,0 +1,35 @@
+package com.emscrm.reportTypes;
+
+import com.emscrm.FuzeQueueSummary;
+
+import java.util.Map;
+
+public class ContractorSupportQS extends FuzeQueueSummary {
+    public static final String reportName = "SC Contractor monthly";
+
+    protected final Map<String, String> tableNames = Map.of("Report Totals", "ContractorTable2019");
+
+    public ContractorSupportQS() {
+        super.weeklyReportFilename = "s:\\reports\\call Centers\\service channel\\service channel monthly 2019.xlsx";
+    }
+
+    @Override
+    protected Map<String, String> getTargetTableNames() {
+        return tableNames;
+    }
+
+    @Override
+    protected String getWeeklyReportFilename() {
+        return weeklyReportFilename;
+    }
+
+    @Override
+    protected boolean isSingleLineTable() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return reportName;
+    }
+}
